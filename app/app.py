92c520c5872564937c455c47d2f3e80de9d934b8
ryuuseinan <galvezluis72@gmail.com>
2023-04-26 01:02:51 -0400

    Código comentado

--- app/app.py
+++ app/app.py
@@ -1,13 +1,20 @@
-from flask import Flask, flash, render_template
+# Importamos las librerías necesarias
+from flask import Flask, render_template
 import secrets
+from database import engine
 
+# Creamos la aplicación Flask
 app = Flask(__name__)
+
+# Generamos una clave secreta aleatoria y la establecemos en la aplicación
 app.secret_key = secrets.token_hex(16)
 
+# Creamos la ruta para la página principal
 @app.route('/')
 def index():
-    flash('Hola, mundo!')
+    # Renderizamos el archivo index.html
     return render_template('index.html')
 
+# Iniciamos la aplicación si se ejecuta este archivo directamente
 if __name__ == '__main__':
     app.run()
