eecb6a76e034eeba871319bd01719708307488b7
ryuuseinan <galvezluis72@gmail.com>
2023-05-23 15:24:52 -0400

    CRUD de Personas

--- app/app.py
+++ app/app.py
@@ -200,7 +200,111 @@ def personas():
     for usuario in personas:
         fecha_creacion = arrow.get(usuario.fecha_creacion).to('America/Santiago').format('DD-MM-YYYY HH:mm') if usuario.fecha_creacion else None
         ultima_modificacion = arrow.get(usuario.ultima_modificacion).to('America/Santiago').format('DD-MM-YYYY HH:mm') if usuario.ultima_modificacion else None
-    return render_template('personas/personas.html', personas=personas, usuario=usuario)
+    return render_template('persona/personas.html', personas=personas, usuario=usuario)
+
+@app.route('/persona/nuevo', methods=['GET', 'POST'])
+def persona_nuevo():
+    error = None
+    usuario = session.query(Usuario).all()
+    if request.method == 'POST':
+        nombre_persona = request.form['nombre_persona']
+        correo = request.form['correo']
+        contrasena = request.form['contrasena']
+        confirmar_contrasena = request.form['confirmar_contrasena']
+        rol_id = request.form['rol_id']
+
+        if contrasena != confirmar_contrasena:
+            error = "Las contraseñas no coinciden"
+        else:
+            contrasena_hash = bcrypt.hashpw(contrasena.encode(), bcrypt.gensalt())
+            persona = persona(nombre_persona=nombre_persona, correo=correo, contrasena=contrasena_hash, rol_id=rol_id)
+            try:
+                session.add(persona)
+                session.commit()
+                flash('El persona ha sido creado exitosamente.', 'success')
+                return redirect(url_for('personas'))
+            except IntegrityError:
+                session.rollback()
+                error = "El nombre de persona o correo electrónico ya están en uso"
+    return render_template('persona/nuevo.html', error=error, rol=rol)
+    
+@app.route('/personas/papelera')
+def persona_papelera():
+    rol = session.query(Rol).all()
+    # Obtenemos todos los personas de la base de datos
+    personas = session.query(persona).all()
+
+    # Verificamos si hay al menos un persona no activo
+    hay_activos = any(persona.activo for persona in personas)
+    i = 0
+    for persona in personas:
+        if not persona.activo:
+            i=i+1
+    if i>=1:
+        hay_activos = True
+    if i == 0:
+        hay_activos = False
+
+    return render_template('persona/papelera.html', personas=personas, hay_activos=hay_activos)
+
+@app.route('/persona/restaurar/<int:id>', methods=['GET', 'POST'])
+def persona_restaurar(id):
+    rol = session.query(Rol).all()
+    persona = session.query(persona).filter_by(id=id).one()
+    if request.method == 'POST':
+        persona.activo = 1
+        session.commit()
+        return redirect(url_for('personas'))
+    else:
+        return render_template('persona/restaurar.html', persona=persona)
+
+@app.route('/persona/editar/<int:id>', methods=['GET', 'POST'])
+def persona_editar(id):
+    rol = session.query(Rol).all()
+    persona = session.query(persona).get(id)
+
+    if not persona:
+        flash('El persona no existe', 'error')
+        return redirect(url_for('personas'))
+
+    if request.method == 'POST':
+        nombre_persona = request.form['nombre_persona']
+        correo = request.form['correo']
+        contrasena = request.form['contrasena']
+        confirmar_contrasena = request.form['confirmar_contrasena']
+        rol_id = request.form['rol_id']
+        
+        # Validar formulario
+        if not nombre_persona:
+            flash('El nombre de persona es requerido', 'error')
+        elif not correo:
+            flash('El correo electrónico es requerido', 'error')
+        elif contrasena != confirmar_contrasena:
+            flash('Las contraseñas no coinciden', 'error')
+        else:
+            # Actualizar persona
+            persona.nombre_persona = nombre_persona
+            persona.correo = correo
+            persona.rol_id = rol_id
+            if contrasena:
+                contrasena_hash = bcrypt.hashpw(contrasena.encode(), bcrypt.gensalt())
+                persona.contrasena = contrasena_hash
+            persona.ultima_modificacion = datetime.now()
+            session.commit()
+            
+            flash('persona actualizado exitosamente', 'success')
+            return redirect(url_for('personas'))
+    return render_template('persona/editar.html', persona=persona, rol=rol)
+
+@app.route('/persona/eliminar/<int:id>', methods=['GET', 'POST'])
+def persona_eliminar(id):
+    persona = session.query(persona).filter_by(id=id).one()
+    if request.method == 'POST':
+        persona.activo = 0
+        session.commit()
+        return redirect(url_for('personas'))
+    else:
+        return render_template('persona/eliminar.html', persona=persona)
 
 @app.route('/usuarios')
 def usuarios():
